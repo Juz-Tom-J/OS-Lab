70bd73b187da2a9cd21afb2ef6c57f2b5366163f
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-05-16 12:22:26 +0530

    Code added

---
+++ Information.sh
@@ -0,0 +1,15 @@
+echo "Enter name: "
+read name
+echo "Enter semester: "
+read semester
+echo "Enter batch: "
+read batch
+echo "Enter roll number: "
+read roll_no
+echo "Enter register number: "
+read reg_no
+echo "Name: $name"
+echo "Semester: $semester"
+echo "Batch: $batch"
+echo "Roll Number: $roll_no"
+echo "Register Number: $reg_no"
