35920feb5396d2c6c858dd086a9ff299622dbe78
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-06-02 15:57:57 +0530

    Code added

---
+++ SystemCall3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<dirent.h>
+#include<stdlib.h>
+
+struct dirent *dptr;
+
+int main(int argc, char *argv[])
+{
+ char buff[256];
+ DIR *dirp;
+ printf("\n\n Enter directory name: ");
+ scanf("%s",buff);
+ if((dirp=opendir(buff))==NULL)
+ {
+  printf("Error");
+  exit(1);
+ }
+ while(dptr=readdir(dirp))
+ {
+  printf("%s\n",dptr->d_name);
+ }
+  closedir(dirp);
+ }
